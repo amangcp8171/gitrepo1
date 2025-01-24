1c7c2321669c511167dfa3b5034ea793beda9155
amangcp8171 <amangcp8171@gmail.com>
2025-01-24 16:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World! Welcome to my Dockerized Web App3'
+    return 'Hello, World! Welcome to my Dockerized Web App1'
 
 
 if __name__ == '__main__':
